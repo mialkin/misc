ba5d014bd85e4f91ff6cb38d05019f0b86230d87
Aleksei Mialkin <anmyalkin@gmail.com>
2021-06-23 23:33:27 +0300

    Add Watchdogs project

--- src/Watchdogs/Startup.cs
+++ src/Watchdogs/Startup.cs
@@ -1,18 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace HealthCheck.Watchdogs
+namespace Watchdogs
 {
     public class Startup
     {
@@ -27,10 +20,11 @@ namespace HealthCheck.Watchdogs
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "HealthChecks.Watchdogs", Version = "v1"});
-            });
+            services
+                .AddHealthChecksUI()
+                .AddInMemoryStorage();
+            
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Watchdogs", Version = "v1"}); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +34,7 @@ namespace HealthCheck.Watchdogs
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HealthChecks.Watchdogs v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Watchdogs v1"));
             }
 
             app.UseHttpsRedirection();
@@ -48,7 +42,7 @@ namespace HealthCheck.Watchdogs
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseHealthChecksUI(config => config.UIPath = "/hc-ui");
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
