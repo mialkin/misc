811052fc6d7b2b206cf4023f30d003f7ef54a4bc
Aleksei Mialkin <anmyalkin@gmail.com>
2021-06-23 22:40:04 +0300

    Add AspNetCore.HealthChecks.UI NuGet package

--- src/HealthChecks.Ui/Startup.cs
+++ src/HealthChecks.Ui/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace HealthChecks.Ui
@@ -31,6 +24,7 @@ namespace HealthChecks.Ui
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "HealthChecks.Ui", Version = "v1"});
             });
+            services.AddHealthChecksUI();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
